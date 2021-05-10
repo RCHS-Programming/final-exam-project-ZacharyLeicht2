08edd1919ac0adc5a8d1c4ec86922e94ceddc31a
ZacharyLeicht2 <75800317+ZacharyLeicht2@users.noreply.github.com>
2021-05-10 12:30:30 -0400

    Added Objects, Vehicles, Background, Cont. to add more. Just getting Started!

---
+++ GreyCar.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class GreyCar here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class GreyCar extends Actor
+{
+    /**
+     * Act - do whatever the GreyCar wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
